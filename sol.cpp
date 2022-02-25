7fc8e9a4fdad90a9cea2fb7201373b887e35d7a1
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-02-25 14:23:06 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		if (n == 2) {
+			cout << -1 << '\n';
+			continue;
+		}
+		// print the odd numbers first, then print the remaining even numbers
+		// note: all numbers must be within this range (1 <= A[i] <= (N * N))
+		int odd = 1;
+		int even = 2;
+		int last = n * n;
+		int a[n][n] = {0};
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (odd <= last) {
+					a[i][j] = odd;
+					odd += 2;
+				} else {
+					a[i][j] = even;
+					even += 2;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << a[i][j] << " ";
+			}
+			cout << '\n';
+		}
+  }	
+	return 0;
+}
